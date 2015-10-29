ea9632d0440b9c028ba6218737c2a908fa57dd19
Grzegorz Kotlarz <g.kotlarz95@gmail.com>
2015-10-29 02:21:09 +0100

    produkt - model z properties

--- ProduktyWebSerwis/Models/Produkt.cs
+++ ProduktyWebSerwis/Models/Produkt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace ProduktyWebSerwis.Models
 {
     public class Produkt
     {
+        [Display(Name="Id produktu")]
+        public int Id { get; set; }
+
+        public virtual ICollection<Allergen> Allergeny { get; set; }
+
+        [Display(Name ="Nazwa produktu")]
+        public string Nazwa { get; set; }
+
+        public virtual Kategoria Kategoria { get; set; }
+
     }
 }
