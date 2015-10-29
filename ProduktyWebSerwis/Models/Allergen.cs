6387da1e36f011a3f3d37ebe7585019696794d99
Grzegorz Kotlarz <g.kotlarz95@gmail.com>
2015-10-29 02:27:19 +0100

    allergen - model z properties

--- ProduktyWebSerwis/Models/Allergen.cs
+++ ProduktyWebSerwis/Models/Allergen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace ProduktyWebSerwis.Models
 {
     public class Allergen
     {
+        [Display(Name="Id allergenu:")]
+        public int Id { get; set; }
 
+        [Display(Name ="Nazwa allergenu:")]
+        public string Nazwa { get; set; }
     }
 }
