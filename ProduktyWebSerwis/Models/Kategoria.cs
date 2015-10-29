b6a8b7ab2561dfb13d764dcea8ff9a2fd9a705b8
Grzegorz Kotlarz <g.kotlarz95@gmail.com>
2015-10-29 02:30:01 +0100

    kategoria - model z properties

--- ProduktyWebSerwis/Models/Kategoria.cs
+++ ProduktyWebSerwis/Models/Kategoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace ProduktyWebSerwis.Models
 {
     public class Kategoria
     {
+        [Display(Name="Id kategorii:")]
+        public int Id { get; set; }
+
+        [Display(Name ="Nazwa kategorii:")]
+        public string Nazwa { get; set; }
     }
 }
