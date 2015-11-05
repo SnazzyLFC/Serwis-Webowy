bab3a15567f21839ce575208abb7fb3cd9048f10
Grzegorz Kotlarz <g.kotlarz95@gmail.com>
2015-11-05 23:02:06 +0100

    repo added

---
+++ ProduktyWebSerwis/Repo/ProduktRepo.cs
@@ -0,0 +1,20 @@
+﻿using ProduktyWebSerwis.Models;
+using ProduktyWebSerwis.Repo.IRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProduktyWebSerwis.Repo
+{
+    public class ProduktRepo : IProduktRepo
+    {
+        private ProdContext db = new ProdContext();
+
+        public IQueryable<Produkt> PobierzProdukty()
+        {
+            return db.Produkty.AsNoTracking();
+        }
+    }
+}
