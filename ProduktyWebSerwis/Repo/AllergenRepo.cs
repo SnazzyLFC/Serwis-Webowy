394d228d1d8325b4355859fc7024931d2f1f7142
Grzegorz Kotlarz <g.kotlarz95@gmail.com>
2015-11-12 19:29:46 +0100

    changes in create

---
+++ ProduktyWebSerwis/Repo/AllergenRepo.cs
@@ -0,0 +1,30 @@
+﻿using ProduktyWebSerwis.Repo.IRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProduktyWebSerwis.Models;
+
+namespace ProduktyWebSerwis.Repo
+{
+    public class AllergenRepo : IAllergenRepo
+    {
+        public AllergenRepo(IProdContext db)
+        {
+            _db = db;
+        }
+        private readonly IProdContext _db;
+
+        public List<Allergen> PobierzAllergeny()
+        {
+            var allergeny = _db.Allergeny.AsNoTracking();
+            var ListaAllergen = new List<Allergen>();
+            foreach(var allergen in allergeny)
+            {
+                ListaAllergen.Add(allergen);
+            }
+            return ListaAllergen;
+        }
+    }
+}
